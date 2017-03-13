7a78aa038ce9c61706d548807c2d21225f4ff8c7
lillieogden <lillieogden@comcast.net>
2017-03-13 15:43:37 -0600

    Added crop function and made other small changes

--- process_data.py
+++ process_data.py
@@ -8,7 +8,7 @@ FILENAMES = [
     'TTM01b_ADVbottom_NREL01_June2014',
     'TTM01_ADVtop_NREL02_June2014',
     'TTM01_ADVbottom_NREL01_June2014'
-]
+    ]
 
 
 def load_data(filename):
@@ -16,6 +16,13 @@ def load_data(filename):
     fname = './data_cache/' + filename
     data = avm.load(fname + '.h5')
     return data
+
+
+def crop_data(data):
+    """Crops the data"""
+    t_range_inds = ((data.props['inds_range'][0] < data.mpltime) &
+                    (data.mpltime < data.props['inds_range'][1]))
+    data.subset(t_range_inds)
 
 
 def save_h5(data, filename):
@@ -36,9 +43,11 @@ if __name__ == '__main__':
 
     for filename in FILENAMES:
         data = avm.read_nortek(filename + '.VEC')
-        avm.clean.GN2002(data)
+        save_h5(data, filename + '_raw')
+        crop_data(data)
         avm.motion.correct_motion(data, accel_filter)
         avm.rotate.earth2principal(data)
-        save_h5(data, filename)
+        avm.clean.GN2002(data)
+        save_h5(data, filename + '_processed')
         dat_bin = bin_data(data)
         save_h5(dat_bin, filename + '_binned')
