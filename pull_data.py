d4f769eeddbec5d5aafd72692620a13feb4003dc
Levi Kilcher <Levi.Kilcher@nrel.gov>
2017-03-13 14:15:09 -0600

    Each file a different row, and simplify URLs.

--- pull_data.py
+++ pull_data.py
@@ -1,12 +1,18 @@
 import urllib2
 
-FILENAMES = ['TTM_NREL03_May2015', 'TTM_NRELvector_Jun2012', 'TTM01b_ADVbottom_NREL01_June2014',
-             'TTM01_ADVtop_NREL02_June2014', 'TTM01_ADVbottom_NREL01_June2014']
-URLS = ['https://mhkdr.openei.org/files/51/TTM_NREL03_May2015.VEC',
-        'https://mhkdr.openei.org/files/49/AdmiraltyInlet_June2012.VEC',
-        'https://mhkdr.openei.org/files/50/ttm01b_ADVbottom_NREL01_June2014.VEC',
-        'https://mhkdr.openei.org/files/50/ttm01_ADVtop_NREL02_June2014.VEC',
-        'https://mhkdr.openei.org/files/50/ttm01_ADVbottom_NREL01_June2014.VEC']
+FILENAMES = ['TTM_NREL03_May2015',
+             'TTM_NRELvector_Jun2012',
+             'TTM01b_ADVbottom_NREL01_June2014',
+             'TTM01_ADVtop_NREL02_June2014',
+             'TTM01_ADVbottom_NREL01_June2014']
+
+base_url = 'https://mhkdr.openei.org/files/'
+
+URLS = ['51/TTM_NREL03_May2015.VEC',
+        '49/AdmiraltyInlet_June2012.VEC',
+        '50/ttm01b_ADVbottom_NREL01_June2014.VEC',
+        '50/ttm01_ADVtop_NREL02_June2014.VEC',
+        '50/ttm01_ADVbottom_NREL01_June2014.VEC']
 
 
 def download(filename, url):
@@ -17,6 +23,5 @@ def download(filename, url):
 
 for item in zip(FILENAMES, URLS):
     print 'Starting download'
-    download(item[0], item[1])
     print 'File successfully downloaded'
-
+    download(item[0], base_url + item[1])
